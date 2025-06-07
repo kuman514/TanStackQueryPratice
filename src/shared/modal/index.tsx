620093f57ecc8056c21496f88fba6eef6b7e07e0
kuman514 <hadjadj0@gmail.com>
2025-06-08 01:36:22 +0900

    fix: correct modal styles

--- src/shared/modal/index.tsx
+++ src/shared/modal/index.tsx
@@ -7,8 +7,8 @@ interface Props {
 
 export default function Modal({ children }: Props) {
   return createPortal(
-    <div className="fixed left-0 top-0 w-screen h-dvh bg-[rgba(0,0,0,0.5)] flex justify-center align-center">
-      <div className="p-8 border border-solid border-green-500 bg-white text-black">
+    <div className="fixed left-0 top-0 w-screen h-dvh bg-[rgba(0,0,0,0.5)] flex justify-center items-center">
+      <div className="p-8 border border-solid border-green-500 bg-white text-black rounded-xl">
         {children}
       </div>
     </div>,
