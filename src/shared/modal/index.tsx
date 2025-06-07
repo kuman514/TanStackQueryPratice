d32ad1522153864d5f1dee9d9ae9bcf308e425f8
kuman514 <hadjadj0@gmail.com>
2025-06-08 03:44:19 +0900

    fix: add click-outside event handler

--- src/shared/modal/index.tsx
+++ src/shared/modal/index.tsx
@@ -3,12 +3,22 @@ import { createPortal } from 'react-dom';
 
 interface Props {
   children?: ReactNode;
+  onClickOutside: () => void;
 }
 
-export default function Modal({ children }: Props) {
+export default function Modal({ children, onClickOutside }: Props) {
   return createPortal(
-    <div className="fixed left-0 top-0 w-screen h-dvh bg-[rgba(0,0,0,0.5)] flex justify-center items-center">
-      <div className="p-8 border border-solid border-green-500 bg-white text-black rounded-xl">
+    <div
+      className="fixed left-0 top-0 w-screen h-dvh bg-[rgba(0,0,0,0.5)] flex justify-center items-center"
+      onClick={onClickOutside}
+    >
+      <div
+        className="p-8 border border-solid border-green-500 bg-white text-black rounded-xl"
+        onClick={(event) => {
+          event.preventDefault();
+          event.stopPropagation();
+        }}
+      >
         {children}
       </div>
     </div>,
